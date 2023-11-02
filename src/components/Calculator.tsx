abb2c41fccbe9f51d3b0d91d89dbde693b71b9e9
Prashanth Balakrishnan <prashbala13@gmail.com>
2023-11-02 18:00:46 -0400

    project complete

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,7 +1,14 @@
 import Input from "./Input";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import iconArrow from "../assets/icon-arrow.svg";
+
+import { useState } from "react";
 const Calculator = () => {
+  const [validDay, setValidDay] = useState(true);
+  const [validMonth, setValidMonth] = useState(true);
+  const [validYear, setValidYear] = useState(true);
+
+  const [age, setAge] = useState({ years: "--", months: "--", days: "--" });
   const {
     register,
     handleSubmit,
@@ -14,9 +21,52 @@ const Calculator = () => {
     },
   });
 
+  // Validates and Calculates the age
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    console.log(data);
+    const { day, month, year } = data;
+
+    const birthYear = year;
+    const birthMonth = month - 1;
+    const birthDay = day;
+
+    if (birthDay > 31 || birthDay < 1) {
+      setValidDay(false);
+    } else {
+      setValidDay(true);
+    }
+    if (birthMonth > 12 || birthMonth < 1) {
+      setValidMonth(false);
+    } else {
+      setValidMonth(true);
+    }
+    if (birthYear > new Date().getFullYear() || birthYear < 1900) {
+      setValidYear(false);
+    } else {
+      setValidYear(true);
+    }
+    if (
+      birthDay > 31 ||
+      birthDay < 1 ||
+      birthMonth > 12 ||
+      birthMonth < 1 ||
+      birthYear > new Date().getFullYear() ||
+      birthYear < 1900
+    ) {
+      return;
+    }
+
+    const today = new Date();
+    const birthDateObj = new Date(birthYear, birthMonth, birthDay);
+
+    const ageInMs = today.getTime() - birthDateObj.getTime();
+    const ageDate = new Date(ageInMs);
+    const years = String(ageDate.getUTCFullYear() - 1970);
+    const months = String(ageDate.getUTCMonth());
+    const days = String(ageDate.getUTCDate() - 1);
+
+    setAge({ years, months, days });
   };
+  console.log(validDay, validMonth, validYear);
   return (
     <div className="calculator">
       <form className="form" onSubmit={handleSubmit(onSubmit)}>
@@ -28,7 +78,8 @@ const Calculator = () => {
             type="number"
             register={register}
             errors={errors}
-            disabled={false}
+            errorMessage="Must be a valid day"
+            isValid={validDay}
           />
           <Input
             id="month"
@@ -37,7 +88,8 @@ const Calculator = () => {
             type="number"
             register={register}
             errors={errors}
-            disabled={false}
+            errorMessage="Must be a valid month"
+            isValid={validMonth}
           />
           <Input
             id="year"
@@ -46,7 +98,8 @@ const Calculator = () => {
             type="number"
             register={register}
             errors={errors}
-            disabled={false}
+            errorMessage="Must be in the past"
+            isValid={validYear}
           />
         </div>
         <div className="form__line">
@@ -57,15 +110,15 @@ const Calculator = () => {
       </form>
       <div className="result">
         <div className="result__container">
-          <span className="result__num">10</span>
+          <span className="result__num">{age.years}</span>
           <span className="result__name">years</span>
         </div>
         <div className="result__container">
-          <span className="result__num">3</span>
+          <span className="result__num">{age.months}</span>
           <span className="result__name">months</span>
         </div>
         <div className="result__container">
-          <span className="result__num">10</span>
+          <span className="result__num">{age.days}</span>
           <span className="result__name">days</span>
         </div>
       </div>
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -10,38 +10,48 @@ interface InputProps {
   register: UseFormRegister<FieldValues>;
   errors: FieldErrors;
   disabled?: boolean;
+  errorMessage?: string;
+  isValid?: boolean;
 }
 
 const Input: React.FC<InputProps> = ({
   label,
   id,
   type,
-  required,
   register,
   errors,
   disabled,
   placeholder,
+  isValid,
+  errorMessage,
 }) => {
   return (
     <div className="input">
-      <label className="input__label" htmlFor={id}>
+      <label
+        className={clsx("input__label", !isValid && "invalid")}
+        htmlFor={id}
+      >
         {label}
       </label>
       <div className="input__wrapper">
         <input
-          className={clsx(
-            "input__field",
-            errors[id] && "input__field--error",
-            disabled && "input__field--disabled"
-          )}
+          className={clsx("input__field", !isValid && "input__field--error")}
           id={id}
           type={type}
           autoComplete={id}
           disabled={disabled}
           placeholder={placeholder}
-          {...register(id, { required })}
+          {...register(id, {
+            required: "This field is required",
+          })}
         />
       </div>
+
+      {errors[id] ? (
+        <p className="input__error">This field is required</p>
+      ) : !isValid ? (
+        <p className="input__error">{errorMessage}</p>
+      ) : null}
     </div>
   );
 };
